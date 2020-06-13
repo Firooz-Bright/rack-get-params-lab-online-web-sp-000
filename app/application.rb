65564adda17f404971eaa0822e2d3a68cb790acb
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-06-13 13:22:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,12 +17,14 @@ class Application
       
        elsif req.path.match(/add/)
          search_term = req.params["item"]
+         
          if @@items.include?(search_term)
            @@cart << search_term
             resp.write "added #{search_term}"
            else
           resp.write "We don't have that item"
            end
+           
      elsif req.path.match(/search/)
         search_term = req.params["q"]
          resp.write handle_search(search_term)
